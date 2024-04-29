a0a2859612c97a3d07f5b1391fa872bb991bcae1
Rahul <rahul@icodelabs.co>
2024-04-29 14:10:42 +0530

    package.json file removed

--- example/views.py
+++ example/views.py
@@ -23,7 +23,7 @@ def index(request):
   html = f'''
   <html>
       <body>
-          <h1>Hello from Vercel!</h1>
+          <h1>Hello! App running successfully</h1>
           <p>The current time is { now }.</p>
       </body>
   </html>
